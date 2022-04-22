5c3ddbee80dfa53b92653a923f1e5a22d6242ba9
saalim786 <saalimsalu300@gmail.com>
2022-04-22 16:20:40 +0530

    arth.sh

---
+++ arth.sh
@@ -0,0 +1,12 @@
+echo "arithmatic operation"
+echo "enter 2 numbers"
+read a b
+s=`expr $a + $b`
+su=`expr $a - $b`
+p=`expr $a \* $b`
+d=`expr $a / $b`
+echo "sum is $s"
+echo "subtraction is $su"
+echo "product is $p"
+echo "division is $d"
+ 
